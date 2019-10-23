73e563fe8621baec807e7d09b3ab50cea842c05f
musafir928 <uganoghli@gmail.com>
2019-10-23 21:29:34 +0200

    week4

---
+++ precache-manifest.50f0c87894f04da7bcefb1af7c690dab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7343de9c37f0863769c662cf5d3ac9a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c65b0d1586bdf75a944c",
+    "url": "/static/css/main.1fc37937.chunk.css"
+  },
+  {
+    "revision": "488834a045902c84c101",
+    "url": "/static/js/2.45b670c4.chunk.js"
+  },
+  {
+    "revision": "c65b0d1586bdf75a944c",
+    "url": "/static/js/main.6c653576.chunk.js"
+  },
+  {
+    "revision": "722362a7198e7580534e",
+    "url": "/static/js/runtime-main.3cdc0666.js"
+  }
+]);
